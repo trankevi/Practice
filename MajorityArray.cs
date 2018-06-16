f19db106f345b882ef00e269ece736867ce35dc8
trankevi <k.tran@alumni.ubc.ca>
2018-06-15 21:54:24 -0700

    added Majority question

---
+++ MajorityArray.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/*
+ * Problem:
+ * Given an unsorted array of integers which has a number in the majority
+ * (appears more than 50% in the array), write a method to find that number
+ * 
+ * e.g. int[] array = { 1, 3, 3, 4, 2, 3, 7, 3, 3 };
+ * We would want the method to return 3 in this case as it appears 5/9 times.
+ */
+
