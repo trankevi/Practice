53e7ac9e7a12cb3b586f5804502ff4f07d763359
trankevi <k.tran@alumni.ubc.ca>
2018-06-16 23:49:03 -0700

    added Prime question

---
+++ Prime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/*
+ * Problem:
+ * Write a method that takes an integer input and determines if that inputted 
+ * integer is a prime number (factors are only 1 and itself); the method should
+ * return the corresponding boolean.
+ * 
+ * e.g. input of 7 would return true as its only factors are 1 and itself
+ *      input of 12 would return false as it has other factors (12,6,4,3,2,1)
+ */
+
