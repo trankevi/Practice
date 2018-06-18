8660782520fe6f7327264c08f850d775e8356db4
trankevi <k.tran@alumni.ubc.ca>
2018-06-17 23:36:49 -0700

    added remove string duplicates problem

---
+++ RemoveStringDupes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+/*
+ * Problem:
+ * Write a method that takes an input string and outputs a string with all the
+ * duplicate characters removed.
+ * 
+ * e.g. string input = "aaaaaabbbb"
+ * We would want the method to return just "ab"
+ */
