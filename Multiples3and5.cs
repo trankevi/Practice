35bcc4d97fc47f67f832d3c7358a25c2feacf2c5
trankevi <k.tran@alumni.ubc.ca>
2018-06-20 19:22:17 -0700

    added solution to multiplesof3and5

--- Multiples3and5.cs
+++ Multiples3and5.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;//for the Dictionary (hashmap)
 
 /*
  * Problem:
@@ -8,3 +9,44 @@
  * 
  * source: https://projecteuler.net/problem=1
  */
+
+class Multiples3and5
+{
+
+    //Current Solution adds multiples to a hashmap and sums the multiples. Could be improved. 
+    //Will add further explanation/optimizations in future.
+
+    static void GetMultiples(int bar, int[] numbers, Dictionary<int, int> multiplelist){
+        foreach (int num in numbers) {
+            for (int i = 1; i < bar; i++){
+                int current = num * i;
+                if (current >= bar){
+                    break;
+                }
+                if (!multiplelist.ContainsKey(current)){
+                    multiplelist.Add(current, current);
+                }
+            }
+        }
+    }
+
+    static int SumMultiples(Dictionary<int, int> multiplelist){
+        int sum = 0; 
+        foreach (KeyValuePair<int, int> pair in multiplelist){
+            sum += pair.Key;
+        }
+        return sum;
+    }
+
+    static void Main(){
+        int bar = 1000;
+        int[] numbers = { 3, 5 };
+        int sum = 0;
+        Dictionary<int, int> multiplelist = new Dictionary<int, int>();
+
+        GetMultiples(bar, numbers, multiplelist);
+        sum = SumMultiples(multiplelist);
+        Console.WriteLine("The sum is " + sum);
+    }
+        
+}
