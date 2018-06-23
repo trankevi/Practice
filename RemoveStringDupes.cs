471b027b0cb551500927ffb6a90c8f741218834e
trankevi <k.tran@alumni.ubc.ca>
2018-06-23 15:33:17 -0700

    added solution to RemoveStringDupes problem

--- RemoveStringDupes.cs
+++ RemoveStringDupes.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;//for the Dictionary (hashmap)
+using System.Text;//for StringBuilder
 
 /*
  * Problem:
@@ -8,3 +10,33 @@
  * e.g. string input = "aaaaaabbbb"
  * We would want the method to return just "ab"
  */
+
+class RemoveStringDupe{
+    static string RemoveDuplicates(string str){
+        Dictionary<char, int> list = new Dictionary<char, int>();
+        StringBuilder builder = new StringBuilder();
+        string output;
+
+        foreach(char c in str){
+            if (!list.ContainsKey(c)){
+                list.Add(c, 1);
+            }
+        }
+
+        foreach(KeyValuePair<char, int> pair in list){
+            builder.Append(pair.Key);
+        }
+
+        output = builder.ToString();
+        return output;
+    }
+
+    static void Main()
+    {
+        string input = "aaaaaabbbb";
+        string output;
+        output = RemoveDuplicates(input);
+
+        Console.WriteLine("The input " + input + " without duplicates is: " + output);
+    }
+}
